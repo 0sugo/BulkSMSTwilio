491a683288c180db5a4b581a99f86b94529253e0
Joseck Osugo <josugo26@gmail.com>
2024-07-21 23:59:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,12 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+q
+q
+q
+q
+q
+q
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
