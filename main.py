9ec8688d069a786a2762cd7678baa1dd457d657e
Joseck Osugo <josugo26@gmail.com>
2024-07-21 23:59:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 q
-q
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
